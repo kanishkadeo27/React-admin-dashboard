b44e667f2279aae27ad08ecf6fde441c0db6a344
kanishkadeo27 <kanishkadeo92022@gmail.com>
2024-12-26 17:29:43 +0530

    new product page ui done

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,8 +1,35 @@
+import { Publish } from '@mui/icons-material'
 import './newProduct.css'
 
 const NewProduct = () => {
   return (
-    <div className='newProduct'>NewProduct</div>
+    <div className='newProduct'>
+      <h1 className="newProductTitle">
+        New Product
+      </h1>
+      <form className="newProductForm">
+        <div className="addProductItem">
+          <label>Image</label>
+          <input type="file" name="" id="file" />
+        </div>
+        <div className="addProductItem">
+          <label>Name</label>
+          <input type="text" placeholder='Apple Airpods' />
+        </div>
+        <div className="addProductItem">
+          <label>Stock</label>
+          <input type="text" placeholder='123' />
+        </div>
+        <div className="addProductItem">
+          <label>Active</label>
+          <select name="active" id="active">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+        <button className="newProductBtn">Create</button>
+      </form>
+    </div>
   )
 }
 
